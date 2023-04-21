e20cf7a5f033a87cf5ff0cc926e9e1e2d75ad9b5
Armando Rivera <rivera.armando997@gmail.com>
2023-04-21 01:18:45 -0500

    add search query by url

--- src/ui/TodoSearch/index.js
+++ src/ui/TodoSearch/index.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import './TodoSearch.css';
+import React, { useEffect } from "react";
+import "./TodoSearch.css";
+import { useSearchParams } from "react-router-dom";
 
 function TodoSearch({ searchValue, setSearchValue, loading }) {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const querySearched = searchParams.get("search");
+
+  useEffect(() => {
+    setSearchValue(querySearched || "");
+  }, []);
+
   const onSearchValueChange = (event) => {
-    console.log(event.target.value);
-    setSearchValue(event.target.value);
+    const query = event.target.value;
+    setSearchParams({ search: query });
+    setSearchValue(query);
   };
 
   return (
